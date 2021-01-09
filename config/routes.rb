90ca1f99942c8a49bd174821447d1fdd7c45189f
LE-HU <wojciechlech1337@gmail.com>
2021-01-09 22:27:03 +0100

    create authors controller, inherit posts from author

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  devise_for :authors
   root to: 'home#index'
+  devise_for :authors
+  scope module: 'authors' do
+    resources :posts
+  end
 end
